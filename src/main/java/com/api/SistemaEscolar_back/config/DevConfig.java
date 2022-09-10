4a30675019b4a1da4959821a145cf4eb9a9d5b40
Ana Paula <anapaulacff@outlook.com>
2022-09-10 16:12:16 -0300

    perfil desenvolvimento

--- src/main/java/com/api/SistemaEscolar_back/config/DevConfig.java
+++ src/main/java/com/api/SistemaEscolar_back/config/DevConfig.java
@@ -1,24 +1,28 @@
 package com.api.SistemaEscolar_back.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.api.SistemaEscolar_back.services.DBService;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
- 
-    @Value("create")
-    private String value;
 
-    @Bean
-    public boolean instanciaDB(){
-        if(value.equals("create")) {
-            /*this.dbService.instanciaDB();*/
-            System.err.println(value);
-        }
-        return false;
-    }
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	@Bean
+	public boolean instanciaDB() {
+		if(value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		return false;
+	}
 }
