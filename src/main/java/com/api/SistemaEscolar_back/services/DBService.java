f591bd3ce3a96c412870b265ab96bfeb715d1214
Ana Paula <anapaulacff@outlook.com>
2022-09-10 16:18:35 -0300

    autorização JWT

--- src/main/java/com/api/SistemaEscolar_back/services/DBService.java
+++ src/main/java/com/api/SistemaEscolar_back/services/DBService.java
@@ -27,22 +27,20 @@ public class DBService {
     
     @Autowired
     private NotaRepository notaRepository;
-
-
-    //Método de Injeção de Dados no Banco.
+    
+    
     public void instanciaDB(){
 
        
-
 
         Professor pf1 = new Professor(null, "Albaniag", "620.183.150-99","al.baniang@gmail.com","albinha321", "Ciência da computação");
         Professor pf2 = new Professor(null, "Handel", "116.950.590-26","handel.almeidag@gmail.com","handellednah", "Redes de computadores");
 
         Turma tr1 = new Turma(null, "Javeleiro avançado", pf2);
-
-
-        Aluno alu1 = new Aluno(null, "kerolen", "664.289.720-00","kerolen.almeidag@gmail.com","kerolenn11", tr1, null);
-        Aluno alu2 = new Aluno(null, "Rayanna", "257.881.280-21","Rayanna.almeidag@gmail.com","Rayanna123456", tr1, null);
+        
+        Aluno alu1 = new Aluno(null, "Ana Paula", "027.375.971-02","anapaula@gmail.com","AnaPaula", tr1, null);
+        Aluno alu2 = new Aluno(null, "kerolen", "664.289.720-00","kerolen.almeidag@gmail.com","kerolenn11", tr1, null);
+        Aluno alu3 = new Aluno(null, "Rayanna", "257.881.280-21","Rayanna.almeidag@gmail.com","Rayanna123456", tr1, null);
    
         Nota nt1 = new Nota(null, "10", null);
        
